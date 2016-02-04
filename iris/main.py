fc2f2fe81134f6be72986af2ba51b7b521ca70f0
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2016-02-04 13:05:30 +0200

    Application skeleton with Flask

--- iris/main.py
+++ iris/main.py
@@ -1,11 +1,10 @@
-from flask import Flask
-
-print __name__
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return "Hello world!"
-
-app.run(debug = True)
+@app.route('/iris/v1/predict', methods=['POST'])
+def predict():
+  sample = request.json['sample']
+  return jsonify({'response': sample[0]})
+  
+app.run(debug=True, host='0.0.0.0', port=8080)

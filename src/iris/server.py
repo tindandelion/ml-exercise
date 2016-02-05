ba3ae559869ee84d5288ed4f7d7443d24759417a
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2016-02-05 13:33:09 +0200

    Started to work on error handling for incorrect input

--- src/iris/server.py
+++ src/iris/server.py
@@ -1,7 +1,7 @@
 import os.path as path
 import iris.model
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 
 model = iris.model.load()
@@ -9,6 +9,9 @@ app = Flask(__name__)
 
 @app.route('/iris/v1/predict', methods=['POST'])
 def predict():
+    if request.content_type != 'application/json':
+        abort(400)
+
     sample = request.json['sample']
     labels = model.predict([sample])
     return jsonify({'label': int(labels[0])})
--- src/test/test_server_api.py
+++ src/test/test_server_api.py
@@ -9,6 +9,7 @@ def response_json(response):
     return json.loads(response.data.decode("utf-8"))
 
 class IrisApiTest(unittest.TestCase):
+    request_url = '/iris/v1/predict'
 
     def setUp(self):
         self.app = server.app
@@ -19,12 +20,28 @@ class IrisApiTest(unittest.TestCase):
         self.assertEqual(404, response.status_code)
 
     def test_correct_request(self):
-        request_url = '/iris/v1/predict'
         body = to_json({'sample': [1, 2, 3, 4]})
-        response = self.client.post(request_url, content_type="application/json", data=body)
-        print(type(response))
+        response = self.client.post(self.request_url, content_type="application/json", data=body)
+        self.assertValidResponse(response)
 
-        self.assertEqual(200, response.status_code)
-        self.assertEqual({'label': 2}, response_json(response))
+    def test_incorrect_request_invalid_content_type(self):
+        response = self.client.post(self.request_url, content_type="text/plain", data="Blah")
+        self.assertBadRequest(response, "Request Content-Type != application/json")
+
+    def assertBadRequest(self, response, expected_message):
+        self.assertEqual(400, response.status_code, "Response status code is BAD_REQUEST")
+
+
+    def assertValidResponse(self, response):
+        self.assertEqual(200, response.status_code, "Response status code is OK")
+        self.assertEqual("application/json", response.content_type, "Response content type")
+        self.assertTrue(len(response.data) > 0, "Response contains data")
+
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertTrue('label' in data, "Response data contains label field")
+
+        label = data['label']
+        self.assertTrue(type(label) == int, "Label is integer")
+
 
 

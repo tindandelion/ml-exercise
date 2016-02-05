cb157f5e931829badee2e9d06a6b50b309ba9b17
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2016-02-05 14:52:59 +0200

    Perform input data validation

--- src/iris/server.py
+++ src/iris/server.py
@@ -2,18 +2,49 @@ import os.path as path
 import iris.model
 
 from flask import Flask, jsonify, request, abort
+from numbers import Number
+
+INVALID_CONTENT_TYPE = "Request Content-Type != application/json"
+MISSING_SAMPLE = "No sample entry in request JSON"
+BAD_SAMPLE = "Sample must be an array of 4 numbers"
+
+SAMPLE_SIZE = 4
+
+class InvalidRequest(Exception):
+    def __init__(self, desc):
+        self.description = desc
 
 
-model = iris.model.load()
 app = Flask(__name__)
+app.model = iris.model.load()
 
 @app.route('/iris/v1/predict', methods=['POST'])
 def predict():
-    if request.content_type != 'application/json':
-        abort(400)
+    sample = extract_sample_from_request(request)
+    validate_sample_data(sample)
 
-    sample = request.json['sample']
-    labels = model.predict([sample])
+    labels = app.model.predict([sample])
     return jsonify({'label': int(labels[0])})
+
+@app.errorhandler(InvalidRequest)
+def invalid_request(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 400
+    return response
   
-  
+
+def extract_sample_from_request(request):
+    if request.content_type != 'application/json':
+        raise InvalidRequest(INVALID_CONTENT_TYPE)
+    if not 'sample' in request.json:
+        raise InvalidRequest(MISSING_SAMPLE)
+
+    return request.json['sample']
+
+def validate_sample_data(sample):
+    if (not isinstance(sample, list)) or len(sample) != SAMPLE_SIZE or not all_numbers(sample): 
+        raise InvalidRequest(BAD_SAMPLE)
+
+def all_numbers(sample):
+    non_numbers = [v for v in sample if not isinstance(v, Number)]
+    return len(non_numbers) == 0
--- src/test/test_server_api.py
+++ src/test/test_server_api.py
@@ -8,29 +8,59 @@ def to_json(data):
 def response_json(response):
     return json.loads(response.data.decode("utf-8"))
 
-class IrisApiTest(unittest.TestCase):
+class IrisInputValidationTest(unittest.TestCase):
     request_url = '/iris/v1/predict'
 
     def setUp(self):
         self.app = server.app
         self.client = self.app.test_client()
 
-    def test_invalid_url(self):
-        response = self.client.get('/')
-        self.assertEqual(404, response.status_code)
-
-    def test_correct_request(self):
+    def test_correct_request_returns_success_response(self):
         body = to_json({'sample': [1, 2, 3, 4]})
         response = self.client.post(self.request_url, content_type="application/json", data=body)
         self.assertValidResponse(response)
 
     def test_incorrect_request_invalid_content_type(self):
         response = self.client.post(self.request_url, content_type="text/plain", data="Blah")
-        self.assertBadRequest(response, "Request Content-Type != application/json")
+        self.assertBadRequest(response, server.INVALID_CONTENT_TYPE)
+
+    def test_incorrect_request_no_sample(self):
+        response = self.client.post(self.request_url, content_type="application/json", data=to_json({}))
+        self.assertBadRequest(response, server.MISSING_SAMPLE)
+
+    def test_incorrect_request_sample_wrong_type(self):
+        data = to_json({'sample': 'rubbish'})
+        response = self.client.post(self.request_url, content_type="application/json", data=data)
+        self.assertBadRequest(response, server.BAD_SAMPLE)
+
+    def test_incorrect_request_sample_wrong_type(self):
+        data = to_json({'sample': 'rubbish'})
+        response = self.client.post(self.request_url, content_type="application/json", data=data)
+        self.assertBadRequest(response, server.BAD_SAMPLE)
+
+    def test_incorrect_request_sample_wrong_sample_size(self):
+        data = to_json({'sample': [0, 0, 0]})
+        response = self.client.post(self.request_url, content_type="application/json", data=data)
+        self.assertBadRequest(response, server.BAD_SAMPLE)
+
+        data = to_json({'sample': [0, 0, 0, 0, 0]})
+        response = self.client.post(self.request_url, content_type="application/json", data=data)
+        self.assertBadRequest(response, server.BAD_SAMPLE)
+
+    def test_incorrect_request_sample_wrong_sample_entry(self):
+        data = to_json({'sample': [0, 0, 0, 'rubbish']})
+        response = self.client.post(self.request_url, content_type="application/json", data=data)
+        self.assertBadRequest(response, server.BAD_SAMPLE)
 
     def assertBadRequest(self, response, expected_message):
         self.assertEqual(400, response.status_code, "Response status code is BAD_REQUEST")
+        self.assertEqual("application/json", response.content_type, "Response content type")
 
+        data = json.loads(response.data.decode("utf-8"))
+        self.assertTrue('message' in data, "Error response  contains 'message' field")
+
+        message = data['message']
+        self.assertEqual(expected_message, message, "Expected error message")
 
     def assertValidResponse(self, response):
         self.assertEqual(200, response.status_code, "Response status code is OK")

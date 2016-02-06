770523573ef211eefc854988a04beef2177224c2
Sergey Moshnikov <s.moshnikov@gmail.com>
2016-02-06 19:21:51 +0200

    Logging config introduced

--- src/main.py
+++ src/main.py
@@ -2,10 +2,5 @@ import logging
 
 from iris.server import app
 
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(name)s|%(asctime)s|%(levelname)s|%(message)s')
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)

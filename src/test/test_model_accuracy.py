d02e29a9d6640c01085f46447a9d417816b43c5c
Sergey Moshnikov <s.moshnikov@gmail.com>
2016-02-07 21:07:15 +0200

    Trying to evaluate model's performance

--- src/test/test_model_accuracy.py
+++ src/test/test_model_accuracy.py
@@ -8,24 +8,31 @@ def load_samples():
     sample_filename = path.join(path.dirname(__file__), "example.json")
     return [s for s in json.load(open(sample_filename)) if 'label' in s]
 
-
 class ModelAccuracyTest(unittest.TestCase):
-    MISMATCH_THRESHOLD = 0.86
+    BASELINE_COST = 0.42
 
     def setUp(self):
         self.model = wm.load()
         self.samples = load_samples()
 
-    def test_prediction_accuracy(self):
-        misses = [sample for sample in self.samples if self.detect_mismatch(sample)]
-        mismatch_ratio = len(misses) / len(self.samples)
-        self.assertLess(mismatch_ratio, self.MISMATCH_THRESHOLD)
+    def test_calculate_performance_cost(self):
+        table = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 0]]
+        totals = [0, 0, 0]
+
+        for smpl in self.samples:
+            label, data = smpl['label'], smpl['info']
+            pred = self.model.predict([data])[0]
+            table[pred][label] += 1
+            totals[label] += 1
+
+        costs = [v/totals[j] for (i, row) in enumerate(table)
+                 for (j, v) in enumerate(row) if i != j]
+
+        self.assertLess(sum(costs), self.BASELINE_COST)
         
 
-    def detect_mismatch(self, sample):
-        label, data = sample['label'], sample['info']
-        predicted = self.model.predict([data])
-        return predicted[0] == label
 
 
 
